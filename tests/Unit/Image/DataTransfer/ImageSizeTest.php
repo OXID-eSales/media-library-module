9ffc13a08700902bd7de5e6457fb92eb8ca3d14d
Anton Fedurtsya <anton@fedurtsya.com>
2023-11-01 18:40:05 +0200

    Add new methods to ImageSize interface and implementation
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- tests/Unit/Image/DataTransfer/ImageSizeTest.php
+++ tests/Unit/Image/DataTransfer/ImageSizeTest.php
@@ -23,4 +23,45 @@ class ImageSizeTest extends TestCase
         $size = new ImageSize(185, 600);
         self::assertEquals(600, $size->getHeight());
     }
+
+    /** @dataProvider isEmptyDataProvider */
+    public function testIsEmpty(int $width, int $height, bool $expected): void
+    {
+        $sut = new ImageSize($width, $height);
+        $this->assertSame($expected, $sut->isEmpty());
+    }
+
+    public function isEmptyDataProvider(): array
+    {
+        return [
+            [0, 0, true],
+            [0, 100, true],
+            [100, 0, true],
+            [10, 10, false],
+        ];
+    }
+
+    /** @dataProvider getInFormatDataProvider */
+    public function testGetInFormat(
+        int $width,
+        int $height,
+        ?string $format,
+        ?string $emptyFormat,
+        string $expectedResult
+    ): void {
+        $sut = new ImageSize($width, $height);
+        $this->assertSame($expectedResult, $sut->getInFormat($format, $emptyFormat));
+    }
+
+    public function getInFormatDataProvider(): array
+    {
+        return [
+            [123, 321, '%dx%d', '', '123x321'],
+            [123, 321, 'Known', '', 'Known'],
+            [0, 0, 'Known', 'Unknown', 'Unknown'],
+            [123, 0, '%dx%d', '%d:%d', '123:0'],
+            [0, 321, '%dx%d', '%d:%d', '0:321'],
+            [0, 321, '%dx%d', '', ''],
+        ];
+    }
 }
