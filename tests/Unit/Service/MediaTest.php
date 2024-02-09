6f5dd82812022c6bb0e14f14545160b3bd8bf4ef
Anton Fedurtsya <anton@fedurtsya.com>
2024-02-09 14:24:07 +0200

    Remove old tests for reimplemented functionality
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- tests/Unit/Service/MediaTest.php
+++ tests/Unit/Service/MediaTest.php
@@ -7,9 +7,6 @@
 
 namespace OxidEsales\MediaLibrary\Tests\Unit\Service;
 
-use Doctrine\DBAL\Connection;
-use org\bovigo\vfs\vfsStream;
-use org\bovigo\vfs\visitor\vfsStreamStructureVisitor;
 use OxidEsales\Eshop\Core\Config;
 use OxidEsales\EshopCommunity\Internal\Framework\Database\ConnectionProviderInterface;
 use OxidEsales\EshopCommunity\Internal\Transition\Adapter\ShopAdapterInterface;
@@ -26,7 +23,6 @@ use OxidEsales\MediaLibrary\Media\Repository\MediaRepositoryInterface;
 use OxidEsales\MediaLibrary\Service\FileSystemService;
 use OxidEsales\MediaLibrary\Service\FileSystemServiceInterface;
 use OxidEsales\MediaLibrary\Service\ModuleSettings;
-use OxidEsales\MediaLibrary\Service\NamingService;
 use OxidEsales\MediaLibrary\Service\NamingServiceInterface;
 use OxidEsales\MediaLibrary\Transput\RequestData\UIRequestInterface;
 use PHPUnit\Framework\TestCase;
@@ -36,92 +32,6 @@ use PHPUnit\Framework\TestCase;
  */
 class MediaTest extends TestCase
 {
-    private const FIXTURE_FILE = 'file.jpg';
-
-    public function testUploadMedia()
-    {
-        $sThumbName = $this->getImageSizeAsString(
-            md5(self::FIXTURE_FILE) . '_thumb_',
-            $this->getSut()->imageResource->getDefaultThumbnailSize()
-        );
-        $sFile2 = 'file_1.jpg';
-        $sThumbName1 = $this->getImageSizeAsString(
-            md5($sFile2) . '_thumb_',
-            $this->getSut()->imageResource->getDefaultThumbnailSize()
-        );
-        $structure['out']['pictures']['ddmedia'] = [
-            self::FIXTURE_FILE => 'some file',
-            'thumbs' => [$sThumbName => 'some file', $sThumbName1 => 'some file'],
-        ];
-        $structure['tmp'] = ['uploaded.jpg' => 'some file'];
-        $directory = vfsStream::setup('root', 0777, $structure);
-        $shopConfigMock = $this->createPartialMock(Config::class, ['getConfigParam']);
-        $shopConfigMock->expects($this->any())
-            ->method('getConfigParam')
-            ->willReturnMap(
-                [
-                    ['sShopDir', null, $directory->url()],
-                    ['sSSLShopURL', null, 'https://test.com'],
-                ]
-            );
-
-        $connectionMock = $this->createPartialMock(
-            Connection::class,
-            [
-                'executeQuery',
-            ]
-        );
-
-        $connectionMock->expects($this->any())
-            ->method('executeQuery');
-
-        $connectionProviderStub = $this->createConfiguredMock(
-            ConnectionProviderInterface::class,
-            [
-                'get' => $connectionMock,
-            ]
-        );
-
-        $sId = md5(self::FIXTURE_FILE);
-
-        $sut = $this->getSut(
-            shopConfig: $shopConfigMock,
-            connectionProvider: $connectionProviderStub,
-            namingService: $this->createPartialMock(NamingService::class, [])
-        );
-
-        $sSourcePath = $directory->url() . '/tmp/uploaded.jpg';
-        $sDestPath = $sut->imageResource->getMediaPath() . self::FIXTURE_FILE;
-        $sFileSize = '1024';
-        $sFileType = 'image/jpeg';
-        $sut->uploadMedia($sSourcePath, $sDestPath, $sFileSize, $sFileType);
-
-        $structureExpected['root'] = [
-            'tmp' => [],
-            'out' => [
-                'pictures' => [
-                    'ddmedia' => [
-                        self::FIXTURE_FILE => 'some file',
-                        $sFile2 => 'some file',
-                        'thumbs' => [
-                            $sThumbName => 'some file',
-                            $sThumbName1 => 'some file',
-                        ],
-                    ],
-                ],
-            ],
-        ];
-        $this->assertEquals(
-            $structureExpected,
-            vfsStream::inspect(new vfsStreamStructureVisitor(), $directory)->getStructure()
-        );
-
-        $sDestPath = $sut->imageResource->getMediaPath() . 'test.js';
-        $sFileType = 'text/javascript';
-        $this->expectException(\Exception::class);
-        $sut->uploadMedia($sSourcePath, $sDestPath, $sFileSize, $sFileType);
-    }
-
     protected function getSut(
         ?ModuleSettings $moduleSettings = null,
         ?Config $shopConfig = null,
