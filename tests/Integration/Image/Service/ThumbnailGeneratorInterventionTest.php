7fc3e7bb1ca91789cbcb108a386535dae5703432
Anton Fedurtsya <anton@fedurtsya.com>
2024-03-07 17:15:41 +0200

    OEVE-211 Improve Intervention thumbnail generator test
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- tests/Integration/Image/Service/ThumbnailGeneratorInterventionTest.php
+++ tests/Integration/Image/Service/ThumbnailGeneratorInterventionTest.php
@@ -11,21 +11,14 @@ use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
 use org\bovigo\vfs\vfsStream;
 use OxidEsales\MediaLibrary\Image\DataTransfer\ImageSize;
-use OxidEsales\MediaLibrary\Tests\Integration\IntegrationTestCase;
 use OxidEsales\MediaLibrary\Image\Service\ThumbnailGeneratorIntervention;
+use OxidEsales\MediaLibrary\Tests\Integration\IntegrationTestCase;
 
 /**
  * @covers \OxidEsales\MediaLibrary\Image\Service\ThumbnailGeneratorIntervention
  */
 class ThumbnailGeneratorInterventionTest extends IntegrationTestCase
 {
-    private $vfsRootUrl;
-    public function setUp(): void
-    {
-        $this->vfsRootUrl = vfsStream::setup()->url();
-        parent::setUp();
-    }
-
     /**
      * @dataProvider getThumbnailDataProvider
      */
@@ -37,15 +30,17 @@ class ThumbnailGeneratorInterventionTest extends IntegrationTestCase
         $expectedThumbnailHeight,
         $cropThumbnail
     ): void {
-        $imageManager = new ImageManager(new Driver());
-        $thumbnailGenerator = new ThumbnailGeneratorIntervention($imageManager);
+        $rootPath = vfsStream::setup()->url();
 
-        $sourcePath = $this->vfsRootUrl . '/source.jpg';
+        $imageManager = new ImageManager(new Driver());
+        $sut = new ThumbnailGeneratorIntervention($imageManager);
+
+        $sourcePath = $rootPath . '/source.jpg';
         $img = $imageManager->create($sourceWidth, $sourceHeight);
         $img->save($sourcePath);
 
-        $thumbnailPath = $this->vfsRootUrl . '/thumbnail.jpg';
-        $thumbnailGenerator->generateThumbnail(
+        $thumbnailPath = $rootPath . '/thumbnail.jpg';
+        $sut->generateThumbnail(
             $sourcePath,
             $thumbnailPath,
             new ImageSize($thumbnailSize, $thumbnailSize),
@@ -59,58 +54,65 @@ class ThumbnailGeneratorInterventionTest extends IntegrationTestCase
         self::assertSame($expectedThumbnailHeight, $resultThumbnailImage->height());
     }
 
-
-    public static function getThumbnailDataProvider()
+    public static function getThumbnailDataProvider(): array
     {
         return [
             'cropped_thumbnail_of_size_800x800_when_bigger_source_size_landscape' => [
-                'sourceWidth'       => 950,
-                'sourceHeight'      => 900,
-                'thumbnailSize'     => 800,
-                'expectedThumbnailWidth'    => 800,
-                'expectedThumbnailHeight'   => 800,
-                'cropThumbnail'             => true,
+                'sourceWidth' => 950,
+                'sourceHeight' => 900,
+                'thumbnailSize' => 800,
+                'expectedThumbnailWidth' => 800,
+                'expectedThumbnailHeight' => 800,
+                'cropThumbnail' => true,
             ],
             'cropped_thumbnail_of_size_500x500_when_smaller_source_size_landscape' => [
-                'sourceWidth'       => 600,
-                'sourceHeight'      => 500,
-                'thumbnailSize'     => 800,
-                'expectedThumbnailWidth'    => 500,
-                'expectedThumbnailHeight'   => 500,
-                'cropThumbnail'             => true,
+                'sourceWidth' => 600,
+                'sourceHeight' => 500,
+                'thumbnailSize' => 800,
+                'expectedThumbnailWidth' => 500,
+                'expectedThumbnailHeight' => 500,
+                'cropThumbnail' => true,
             ],
             'no_cropped_thumbnail_of_size_800x500_when_bigger_source_size_landscape' => [
-                'sourceWidth'       => 800,
-                'sourceHeight'      => 500,
-                'thumbnailSize'     => 800,
-                'expectedThumbnailWidth'    => 800,
-                'expectedThumbnailHeight'   => 500,
-                'cropThumbnail'             => false,
+                'sourceWidth' => 800,
+                'sourceHeight' => 500,
+                'thumbnailSize' => 800,
+                'expectedThumbnailWidth' => 800,
+                'expectedThumbnailHeight' => 500,
+                'cropThumbnail' => false,
             ],
             'no_cropped_thumbnail_of_size_200x300_when_bigger_source_size_potrait' => [
-                'sourceWidth'       => 1000,
-                'sourceHeight'      => 1500,
-                'thumbnailSize'     => 300,
-                'expectedThumbnailWidth'    => 200,
-                'expectedThumbnailHeight'   => 300,
-                'cropThumbnail'             => false,
+                'sourceWidth' => 1000,
+                'sourceHeight' => 1500,
+                'thumbnailSize' => 300,
+                'expectedThumbnailWidth' => 200,
+                'expectedThumbnailHeight' => 300,
+                'cropThumbnail' => false,
             ],
             'no_cropped_thumbnail_of_size_500x100_when_smaller_source_size_landscape' => [
-                'sourceWidth'       => 500,
-                'sourceHeight'      => 100,
-                'thumbnailSize'     => 800,
-                'expectedThumbnailWidth'    => 500,
-                'expectedThumbnailHeight'   => 100,
-                'cropThumbnail'             => false,
+                'sourceWidth' => 500,
+                'sourceHeight' => 100,
+                'thumbnailSize' => 800,
+                'expectedThumbnailWidth' => 500,
+                'expectedThumbnailHeight' => 100,
+                'cropThumbnail' => false,
             ],
             'no_cropped_thumbnail_of_size_500x100_when_smaller_source_size_potrait' => [
-                'sourceWidth'       => 100,
-                'sourceHeight'      => 150,
-                'thumbnailSize'     => 300,
-                'expectedThumbnailWidth'    => 100,
-                'expectedThumbnailHeight'   => 150,
-                'cropThumbnail'             => false,
+                'sourceWidth' => 100,
+                'sourceHeight' => 150,
+                'thumbnailSize' => 300,
+                'expectedThumbnailWidth' => 100,
+                'expectedThumbnailHeight' => 150,
+                'cropThumbnail' => false,
             ],
         ];
+    }
+
+    public function testIsOriginSupported(): void
+    {
+        $imageManager = new ImageManager(new Driver());
+        $sut = new ThumbnailGeneratorIntervention($imageManager);
+
+        $this->assertTrue($sut->isOriginSupported('anything'));
     }
 }
